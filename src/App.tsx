d13879467895692cb460eeb3e80445c08f9e2301
dungtryhard1 <127658265+dungtryhard1@users.noreply.github.com>
2024-10-24 15:01:26 +0700

    finished modal in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import AppRoutes from "./routes/AppRoutes";
+import "./scss/style.scss"
 
 const App: React.FC = () => {
   return (
